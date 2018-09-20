a3db7521069b3569575e572b3961f6d9d4af75f1
李楚源 <lisa27chuyuan@gmail.com>
2018-09-20 18:11:52 +0200

    add python script generator.py

---
+++ generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def read_id_melt():
+    continue
+    # return an pbject of mots cibles (dico??)
+
+
+
+def candidates_fredist():
+    continue
+
+
+
+def candidates_frwak():
+    continue
+
+
+
+
